8eb2fe2c01367c1063bf9ed4c7a835535a2f256c
unknown <u29795@pc9327d15.vw.hs-harz.de>
2016-12-06 14:35:37 +0100

    Basic MainMenu with simple functionality

---
+++ Assets/MenuFunctions.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+using System.Collections;
+
+public class MenuFunctions : MonoBehaviour {
+
+	public void startLevel(int level)
+    {
+        SceneManager.LoadScene(level);
+    }
+
+    public void end()
+    {
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+        Debug.Log("Quitting Editor");
+#else
+        Debug.Log("Quitting App");
+        Application.Quit();
+#endif
+    }
+}
