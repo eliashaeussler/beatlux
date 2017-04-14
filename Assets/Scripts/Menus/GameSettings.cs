6aa68e8d40cb63317c660cccae5b4810f4ab53e3
Kevin Haustein <kevinhaustein16@googlemail.com>
2017-04-14 19:07:58 +0200

    Added code for working options menu, not yet tested

---
+++ Assets/Scripts/Menus/GameSettings.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameSettings : MonoBehaviour {
+    public bool fullscreen;
+    public int textureQuality;
+    public int antialiasing;
+    public int resolutionIndex;
+
+}
---
+++ Assets/Scripts/Menus/SettingManager.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class SettingManager : MonoBehaviour {
+
+    //Referencing all the options from the menu
+    public Toggle fullscreenToggle;
+    public Dropdown resolutionDropdown;
+    public Dropdown textureQualityDropdown;
+    public Dropdown antialiasingDropdown;
+
+    public GameSettings gameSettings;
+
+    //Storage for all available resolutions
+    public Resolution[] resolutions;
+
+    void OnEnable()
+    {
+        gameSettings = new GameSettings();
+
+        fullscreenToggle.onValueChanged.AddListener(delegate { OnFullscreenToggle(); });
+        resolutionDropdown.onValueChanged.AddListener(delegate { OnResolutionChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        textureQualityDropdown.onValueChanged.AddListener(delegate { OnTextureQualityChange(); });
+        antialiasingDropdown.onValueChanged.AddListener(delegate { OnAntialiasingChange(); });
+
+        resolutions = Screen.resolutions;   //Saving all resolutions that the current monitor is able to display
+        foreach(Resolution resolution in resolutions)
+        {
+            resolutionDropdown.options.Add(new Dropdown.OptionData(resolution.ToString()));
+        }
+    }
+
+
+    public void OnFullscreenToggle()
+    {
+       gameSettings.fullscreen = Screen.fullScreen = fullscreenToggle.isOn;
+    }
+
+    public void OnResolutionChange()
+    {
+        Screen.SetResolution(resolutions[resolutionDropdown.value].width, resolutions[resolutionDropdown.value].height, Screen.fullScreen);
+    }
+
+    public void OnTextureQualityChange()
+    {
+        gameSettings.textureQuality = QualitySettings.masterTextureLimit = textureQualityDropdown.value;
+        
+    }
+
+    public void OnAntialiasingChange()
+    {
+        gameSettings.antialiasing = QualitySettings.antiAliasing = (int)Mathf.Pow(2f, antialiasingDropdown.value);
+    }
+
+    public void SaveSettings()
+    {
+
+    }
+
+    public void LoadSettings()
+    {
+
+    }
+}
