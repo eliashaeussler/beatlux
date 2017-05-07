0d91d4ef714e6c277010e671c444806fa4a3bdc8
Kevin Haustein <kevinhaustein16@googlemail.com>
2017-05-07 17:18:42 +0200

    Back button now discards changes and goes back to main menu

--- Assets/Scripts/Menus/GameSettings.cs
+++
@@ -1,6 +0,0 @@
-﻿public class GameSettings {
-    public bool fullscreen;
-    public int textureQuality;
-    public int antialiasing;
-    public int resolutionIndex;
-}
--- Assets/Scripts/Menus/SettingManager.cs
+++ Assets/Scripts/Menus/SettingManager.cs
@@ -102,4 +102,6 @@ public class SettingManager : MonoBehaviour {
 
         }
     }
+
+    
 }
